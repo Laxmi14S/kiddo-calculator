2ce22cf58d564d6e1df39e5743966ccf35997f38
Laxmi14S <laxmisonnad14@gmail.com>
2025-07-22 16:12:53 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,9 +5,8 @@ self.addEventListener("install", event => {
     })
   );
 });
+self.addEventListener('install', e => console.log('SW installed'));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
 });
+
